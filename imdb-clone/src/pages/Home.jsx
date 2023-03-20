9d6c1cdf7d3b716fd20c656599b82700a177fd56
Anshul patle <patleanshul920@gmail.com>
2023-03-21 00:35:08 +0530

    Update Home.jsx

--- imdb-clone/src/pages/Home.jsx
+++ imdb-clone/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+
+// component
+import Header from '../components/common/Header'
+
+
+
 const Home = () => {
   return (
-    <div>hello from home</div>
+    <Header/>
   )
 }
 
