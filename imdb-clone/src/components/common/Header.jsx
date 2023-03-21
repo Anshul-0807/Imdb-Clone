13aa43fa71190a79ad40f39fa2846ecff979fb8f
Anshul patle <patleanshul920@gmail.com>
2023-03-22 00:38:24 +0530

    Update Header.jsx

--- imdb-clone/src/components/common/Header.jsx
+++ imdb-clone/src/components/common/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { AppBar, Toolbar, styled, Box } from '@mui/material';
-import { Menu} from '@mui/icons-material';
+import { AppBar, Toolbar, styled, Box, Typography , InputBase} from '@mui/material';
+import { Menu, BookmarkAdd, ExpandMore} from '@mui/icons-material';
 
 import { logoURL } from '../../constants/constant';
 
@@ -11,7 +11,36 @@ const StyledToolBar = styled(Toolbar)`
     background: #121212;
     min-height: 56px !important;
     padding: 0 115px !important;
+    justify-content: space-between;
+    & > * {
+      padding: 16px;
+    }
+    & > div {
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+      & > p {
+        font-size: 14px;
+        font-weight: 600
+      }
+    }
+    & > p {
+        font-size: 14px;
+        font-weight: 600
+      }
 `;
+
+const InputSearchFeild = styled(InputBase)`
+background: #ffffff;
+height: 30px;
+width: 55%;
+border-radius: 5px;
+
+`;
+
+const Imdbpro = styled(Box)`
+color: #26b5fc;
+`
 
 const Logo = styled("img")({
     width : 64
@@ -24,6 +53,22 @@ const Header = () => {
           <Logo src={logoURL} alt="logo" />
           <Box>
             <Menu/>
+            <Typography>
+              Menu
+            </Typography>
+          </Box>
+          <InputSearchFeild/>
+          <Typography>IMDb<Imdbpro component="span">Pro</Imdbpro></Typography>
+          <Box>
+            <BookmarkAdd/>
+            <Typography>
+              Watchlist
+            </Typography>
+          </Box>
+          <Typography>Sign In</Typography>
+          <Box>
+            <Typography>En</Typography>
+            <ExpandMore/>
           </Box>
         </StyledToolBar>
     </AppBar>
