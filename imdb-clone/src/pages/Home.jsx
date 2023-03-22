e20a29157d52c75b26bc1975172b0f9c5f1d4c56
Anshul patle <patleanshul920@gmail.com>
2023-03-23 01:13:08 +0530

    Update Home.jsx

--- imdb-clone/src/pages/Home.jsx
+++ imdb-clone/src/pages/Home.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-
+import { useEffect, useState } from 'react'
 // component
 import Header from '../components/common/Header'
-
+import { categoryMovies } from '../services/api'
+import { NOWPLAYING_API_URL } from '../constants/constant'
 
 
 const Home = () => {
+
+ const [movies, setMovies] = useState()
+
+  useEffect(() => {
+    const getData = async ([]) => {
+     let response = await categoryMovies(NOWPLAYING_API_URL)
+     setMovies()
+    }
+    getData()
+  }, [])
+  
+
   return (
     <Header/>
   )
