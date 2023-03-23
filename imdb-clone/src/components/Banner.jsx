c5fabe1ea13816df21933c0666d625b7b230d15f
Anshul patle <patleanshul920@gmail.com>
2023-03-24 00:20:15 +0530

    Update Banner.jsx

--- imdb-clone/src/components/Banner.jsx
+++ imdb-clone/src/components/Banner.jsx
@@ -1,37 +1,54 @@
-import { Box } from '@mui/material';
-import React from 'react'
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import { Box, styled } from "@mui/material";
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
-const Banner = () => {
+const StyledBanner = styled("img")({
+  width: "100%",
+});
+
+const Banner = ({ movies }) => {
   return (
-    <Box>
-        <Carousel
-        responsive={responsive}>
-
-        </Carousel>
+    <Box style={{ width: "65%" }}>
+      <Carousel
+        responsive={responsive}
+        swipeable={false}
+        draggable={false}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={3000}
+        keyBoardControl={true}
+        showDots={false}
+        slidesToSlide={1}
+        // containerClass="react-multi-carousel-list"
+        // dotListClass="custom-dot-list-style"
+        // itemClass="carousel-item-padding-40-px"
+        // style={{ overflow: "visible" }}
+      >
+        {movies.map((movie) => (
+          <StyledBanner
+            key={movie.id}
+            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+          />
+        ))}
+      </Carousel>
     </Box>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
