0747d95408d023e8cec27cbf5886e40f3e9a17fa
Anshul patle <patleanshul920@gmail.com>
2023-03-24 23:51:36 +0530

    Update Header.jsx

--- imdb-clone/src/components/common/Header.jsx
+++ imdb-clone/src/components/common/Header.jsx
@@ -1,95 +1,95 @@
-import React from 'react'
-import {useState} from 'react';
-import { AppBar, Toolbar, styled, Box, Typography , InputBase} from '@mui/material';
-import { Menu, BookmarkAdd, ExpandMore} from '@mui/icons-material';
+import React from "react";
+import { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  styled,
+  Box,
+  Typography,
+  InputBase,
+} from "@mui/material";
+import { Menu, BookmarkAdd, ExpandMore } from "@mui/icons-material";
 
-import { logoURL } from '../../constants/constant';
+import { logoURL } from "../../constants/constant";
 
 // component
 
-import HeaderMenu from './HeaderMenu';
-
-
+import HeaderMenu from "./HeaderMenu";
 
 const StyledToolBar = styled(Toolbar)`
-    background: #121212;
-    min-height: 56px !important;
-    padding: 0 115px !important;
-    justify-content: space-between;
-    & > * {
-      padding: 16px;
-    }
-    & > div {
-      display: flex;
-      align-items: center;
-      cursor: pointer;
-      & > p {
-        font-size: 14px;
-        font-weight: 600
-      }
-    }
+  background: #121212;
+  min-height: 56px !important;
+  padding: 0 115px !important;
+  justify-content: space-between;
+  & > * {
+    padding: 16px;
+  }
+  & > div {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
     & > p {
-        font-size: 14px;
-        font-weight: 600
-      }
+      font-size: 14px;
+      font-weight: 600;
+    }
+  }
+  & > p {
+    font-size: 14px;
+    font-weight: 600;
+  }
 `;
 
 const InputSearchFeild = styled(InputBase)`
-background: #ffffff;
-height: 30px;
-width: 55%;
-border-radius: 5px;
-
+  background: #ffffff;
+  height: 30px;
+  width: 55%;
+  border-radius: 5px;
 `;
 
 const Imdbpro = styled(Box)`
-color: #26b5fc;
-`
+  color: #26b5fc;
+`;
 
 const Logo = styled("img")({
-    width : 64
-})
+  width: 64,
+});
 
 const Header = () => {
+  const [open, setOpen] = useState(null);
 
- const [open, setOpen] = useState(null);
+  const handleClick = (e) => {
+    setOpen(e.currentTarget);
+  };
 
-
- const handleClick = (e) => {
-     setOpen(e.currentTarget)
- }
-
- const handleClose = () => {
-  setOpen(null);
-};
+  const handleClose = () => {
+    setOpen(null);
+  };
 
   return (
-    <AppBar position='static'>
-        <StyledToolBar>
-          <Logo src={logoURL} alt="logo" />
-          <Box  onClick={handleClick}>
-            <Menu/>
-            <Typography>
-              Menu
-            </Typography>
-          </Box>
-          <HeaderMenu open={open} handleClose={handleClose}/>
-          <InputSearchFeild/>
-          <Typography>IMDb<Imdbpro component="span">Pro</Imdbpro></Typography>
-          <Box>
-            <BookmarkAdd/>
-            <Typography>
-              Watchlist
-            </Typography>
-          </Box>
-          <Typography>Sign In</Typography>
-          <Box>
-            <Typography>En</Typography>
-            <ExpandMore/>
-          </Box>
-        </StyledToolBar>
+    <AppBar position="static">
+      <StyledToolBar>
+        <Logo src={logoURL} alt="logo" />
+        <Box onClick={handleClick}>
+          <Menu />
+          <Typography>Menu</Typography>
+        </Box>
+        <HeaderMenu open={open} handleClose={handleClose} />
+        <InputSearchFeild />
+        <Typography>
+          IMDb<Imdbpro component="span">Pro</Imdbpro>
+        </Typography>
+        <Box>
+          <BookmarkAdd />
+          <Typography>Watchlist</Typography>
+        </Box>
+        <Typography>Sign In</Typography>
+        <Box>
+          <Typography>En</Typography>
+          <ExpandMore />
+        </Box>
+      </StyledToolBar>
     </AppBar>
-  )
-}
+  );
+};
 
 export default Header;
