bbe3d820c2e0aaef7d81150c98184fa0c1d44e43
Anshul patle <patleanshul920@gmail.com>
2023-03-25 18:07:54 +0530

    Create CategoryMovies.jsx

---
+++ imdb-clone/src/pages/CategoryMovies.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const CategoryMovies = () => {
+  return (
+   <>
+    <Header/>
+    <Box>Hellooo</Box>
+   </>
+  )
+}
+
+export default CategoryMovies
