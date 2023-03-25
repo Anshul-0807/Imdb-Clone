7c28bf162cde8bca84c9c258c44fb03d3b559409
Anshul patle <patleanshul920@gmail.com>
2023-03-25 23:58:55 +0530

    Update CategoryMovies.jsx

--- imdb-clone/src/pages/CategoryMovies.jsx
+++ imdb-clone/src/pages/CategoryMovies.jsx
@@ -1,14 +1,47 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import Header from '../components/common/Header'
+import { Box } from "@mui/material";
+import React from "react";
+import Header from "../components/common/Header";
+import {useState, useEffect} from 'react';
+
 
 const CategoryMovies = () => {
-  return (
-   <>
-    <Header/>
-    <Box>Hellooo</Box>
-   </>
-  )
-}
 
-export default CategoryMovies
+ useEffect(() => {
+  const getData = async () => {
+    await 
+  }
+  getData()
+ }, [third])
+ 
+
+  return (
+    <>
+      <Header />
+      <Box>
+        <Carousel
+          responsive={responsive}
+          swipeable={false}
+          draggable={false}
+          infinite={true}
+          autoPlay={true}
+          autoPlaySpeed={3000}
+          keyBoardControl={true}
+          showDots={false}
+          slidesToSlide={1}
+        >
+          {movies.map((movie) => (
+            <>
+              <StyledBanner
+                key={movie.id}
+                src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              />
+              <Title>{movie.original_title}</Title>
+            </>
+          ))}
+        </Carousel>
+      </Box>
+    </>
+  );
+};
+
+export default CategoryMovies;
