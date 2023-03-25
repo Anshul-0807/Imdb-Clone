385e8af13c91a728e8a565c0ac195416f6531abd
Anshul patle <patleanshul920@gmail.com>
2023-03-25 18:07:47 +0530

    Update UpNext.jsx

--- imdb-clone/src/components/UpNext.jsx
+++ imdb-clone/src/components/UpNext.jsx
@@ -3,12 +3,30 @@ import {Box, Typography, styled} from '@mui/material';
 
 
 const Component = styled(Box)`
-width: 40%
+width: 40%;
+display: flex;
+flex-direction: column;
+align-items: baseline;
+padding-left: 20px;
+& > p {
+  color: #F5C518;
+  font-weight: 600;
+  font-size: 18px;
+  margin-bottom: 10px;
+}
 `;
 
 const Poster = styled('img')({
   width :88
-})
+});
+
+const Wrapper = styled(Box)`
+color: #ffffff;
+display: flex;
+& > p {
+  margin-left: 20px;
+}
+`;
 
 const UpNext = ({movies}) => {
   return (
@@ -16,11 +34,11 @@ const UpNext = ({movies}) => {
      <Typography>Up Next</Typography>
      {
       movies.splice(0,3).map(movie => (
-        <Box>
+        <Wrapper>
           <Poster 
            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="poster" />
            <Typography>{movie.original_title}</Typography>
-        </Box>
+        </Wrapper>
       ))
      }
    </Component>
