da3017691d3c7a87704f5ad9dd22866f95264aa3
Anshul patle <patleanshul920@gmail.com>
2023-04-05 00:18:38 +0530

    Update CategoryMovies.jsx

--- imdb-clone/src/pages/CategoryMovies.jsx
+++ imdb-clone/src/pages/CategoryMovies.jsx
@@ -1,19 +1,16 @@
-import { Box, Typography, styled, Divider } from "@mui/material";
+import { Box , Typography, styled, Divider} from "@mui/material";
 import React from "react";
 import Header from "../components/common/Header";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from 'react';
 import { useLocation } from "react-router-dom";
 import { categoryMovies } from "../services/api";
 import Carousel from "react-multi-carousel";
-import {
-  POPULAR_API_URL,
-  TOPRATED_API_URL,
-  UPCOMING_API_URL,
-  moviestype,
-} from "../constants/constant";
+import { POPULAR_API_URL, TOPRATED_API_URL,UPCOMING_API_URL, moviestype } from "../constants/constant";
 import Movieslist from "../components/Movieslist";
 
+
 // import styled from "@emotion/styled";
+
 
 const responsive = {
   desktop: {
@@ -30,10 +27,10 @@ const responsive = {
   },
 };
 
-const StyledBanner = styled("img")({
+const StyledBanner = styled('img')({
   height: 450,
-  width: "100%",
-});
+  width: '100%'
+})
 
 const Component = styled(Box)`
 width: 80%
@@ -41,33 +38,36 @@ margin: auto;
 `;
 
 const Container = styled(Box)`
-  background: #f5f5f5;
-  padding: 10px;
+ background: #F5F5F5;
+ padding: 10px;
+
 `;
 
 const CategoryMovies = () => {
-  const [movies, setMovies] = useState([]);
+ 
+ const [movies, setMovies] = useState([])
+ 
+ const {search} = useLocation();
 
-  const { search } = useLocation();
+ useEffect(() => {
+  const getData = async (API_URL) => {
+    let response = await categoryMovies(API_URL)
+    setMovies(response.results);
+  }
 
-  useEffect(() => {
-    const getData = async (API_URL) => {
-      let response = await categoryMovies(API_URL);
-      setMovies(response.results);
-    };
+  let API_URL;
 
-    let API_URL;
+  if(search.includes('popular')){
+    API_URL = POPULAR_API_URL;
+  }else if(search.includes('upcoming')){
+    API_URL = UPCOMING_API_URL;
+  }else if(search.includes('toprated')){
+    API_URL= TOPRATED_API_URL;
+  }
 
-    if (search.includes("popular")) {
-      API_URL = POPULAR_API_URL;
-    } else if (search.includes("upcoming")) {
-      API_URL = UPCOMING_API_URL;
-    } else if (search.includes("toprated")) {
-      API_URL = TOPRATED_API_URL;
-    }
-
-    getData(API_URL);
-  }, [search]);
+  getData(API_URL)
+ }, [search])
+ 
 
   return (
     <>
@@ -88,7 +88,7 @@ const CategoryMovies = () => {
             <>
               <StyledBanner
                 key={movie.id}
-                src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
               />
               {/* <Title>{movie.original_title}</Title> */}
             </>
@@ -96,15 +96,10 @@ const CategoryMovies = () => {
         </Carousel>
         <Container>
           <Typography variant="h6">IMDb Charts</Typography>
-          <Typography variant="h4">
-            {" "}
-            IMDb {moviestype[search.split("=")[1]]} movies
-          </Typography>
-          <Typography style={{ fontSize: 12, margin: 5 }}>
-            IMDB Top {movies.length} as rated by regular IMDb voter.{" "}
-          </Typography>
-          <Divider />
-          <Movieslist movies={movies} />
+          <Typography variant="h4"> IMDb {moviestype[search.split('=')[1]]} movies</Typography>
+          <Typography style={{fontSize: 12, margin: 5 }}>IMDB Top {movies.length} as rated by regular IMDb voter. </Typography>
+          <Divider/>
+          <Movieslist movies={movies}/>
         </Container>
       </Component>
     </>
