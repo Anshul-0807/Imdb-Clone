d6ae7a9905243058e936b218f54f257204f1f2fe
Anshul patle <patleanshul920@gmail.com>
2023-04-08 13:10:04 +0530

    Update Home.jsx

--- imdb-clone/src/pages/Home.jsx
+++ imdb-clone/src/pages/Home.jsx
@@ -1,53 +1,50 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 // component
-import Header from '../components/common/Header'
-import { categoryMovies } from '../services/api'
-import { NOWPLAYING_API_URL } from '../constants/constant'
-import { Box, styled } from '@mui/material'
-import Banner from '../components/Banner'
-import UpNext from '../components/UpNext';
-import Slide from '../components/Slide';
-
+import Header from "../components/common/Header";
+import { categoryMovies } from "../services/api";
+import { NOWPLAYING_API_URL } from "../constants/constant";
+import { Box, styled } from "@mui/material";
+import Banner from "../components/Banner";
+import UpNext from "../components/UpNext";
+import Slide from "../components/Slide";
 
 const Wrapper = styled(Box)`
-display: flex;
-padding: 20px 0;
+  display: flex;
+  padding: 20px 0;
 `;
 
 const Component = styled(Box)`
-padding: 0 115px;
+  padding: 0 115px;
 `;
 
 const Home = () => {
-
- const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
-     let response = await categoryMovies(NOWPLAYING_API_URL);
-     setMovies(response.results);
-    }
-    getData()
-  }, [])
-  
+      let response = await categoryMovies(NOWPLAYING_API_URL);
+      setMovies(response.results);
+    };
+    getData();
+  }, []);
 
   return (
     <>
-      <Header/>
-       <Component>
+      <Header />
+      <Component>
         <Wrapper>
-          <Banner movies={movies}/>
-          <UpNext movies={movies}/>         
+          <Banner movies={movies} />
+          <UpNext movies={movies} />
         </Wrapper>
-        <Slide movies={movies}/>
-        <Slide movies={movies}/>
-        <Slide movies={movies}/>
-        <Slide movies={movies}/>
-        <Slide movies={movies}/>
-       </Component>
+        <Slide movies={movies} />
+        <Slide movies={movies} />
+        <Slide movies={movies} />
+        <Slide movies={movies} />
+        <Slide movies={movies} />
+      </Component>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
